80669a36e28f8b5f6dae0c16d29ff910ef4042b1
Ahmed Hossam <hoss4614@gmail.com>
2024-08-05 23:02:20 +0300

    fix deployment

--- restart_everything.sh
+++ restart_everything.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
+# for AWS
+kubectl delete deployment flask-app-deployment
+kubectl delete service flask-app-service
+kubectl delete ingress flask-app-ingress
+kubectl delete pvc json-pvc
+kubectl delete pv json-pv
+
+kubectl apply -f pv.yaml
+kubectl apply -f pvc.yaml
+kubectl apply -f deployment.yaml
+kubectl apply -f service.yaml
+kubectl apply -f ingress.yaml
+
+# for local clusters
 #minikube start
-minikube kubectl -- delete deployment flask-app-deployment
-minikube kubectl -- delete service flask-app-service
-minikube kubectl -- delete ingress flask-app-ingress
-minikube kubectl -- delete pvc json-pvc
-minikube kubectl -- delete pv json-pv
-
-docker build -t ahmedhosssam/flask-app:1722344581 .
-docker push ahmedhosssam/flask-app:1722344581
-
-minikube kubectl -- apply -f pv.yaml
-minikube kubectl -- apply -f pvc.yaml
-minikube kubectl -- apply -f deployment.yaml
-minikube kubectl -- apply -f service.yaml
-minikube kubectl -- apply -f ingress.yaml
+#minikube kubectl -- delete deployment flask-app-deployment
+#minikube kubectl -- delete service flask-app-service
+#minikube kubectl -- delete ingress flask-app-ingress
+#minikube kubectl -- delete pvc json-pvc
+#minikube kubectl -- delete pv json-pv
+#
+#docker build -t ahmedhosssam/flask-app:1722344581 .
+#docker push ahmedhosssam/flask-app:1722344581
+#
+#minikube kubectl -- apply -f pv.yaml
+#minikube kubectl -- apply -f pvc.yaml
+#minikube kubectl -- apply -f deployment.yaml
+#minikube kubectl -- apply -f service.yaml
+#minikube kubectl -- apply -f ingress.yaml
--- terraform-eks/main.tf
+++ terraform-eks/main.tf
@@ -4,10 +4,10 @@ provider "aws" {
 
 locals {
   name   = "team4"
-  region = "us-east-1"
+  region = "eu-central-1"
 
   vpc_cidr = "10.123.0.0/16"
-  azs      = ["us-east-1a", "us-east-1b"]
+  azs      = ["eu-central-1a", "eu-central-1b"]
 
   public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
   private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
